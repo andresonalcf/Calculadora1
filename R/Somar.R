6b7f02208c9e6b96f1310c3c447e5e2b5db574c7
Andreson Costa <alcf2@discente.ifpe.edu.br>
2021-02-26 21:26:54 -0300

    Firt commit de calclualdora1

---
+++ R/Somar.R
@@ -0,0 +1,16 @@
+#'Função para somar
+#'
+#'Esta função serve para somar dois números
+#'
+#'@param a um número a ser somado
+#'@param b outro número a ser somado
+#'@examples
+#'somar (1,2)
+#'resposta 3
+#'
+#'
+#'@export
+somar <- function(a,b){
+  a+b
+}
+
---
+++ R/Subtrair.R
@@ -0,0 +1,16 @@
+#'Função para subtrair
+#'
+#'Esta função serve para subtrair dois números
+#'
+#'@param a um número a ser subtraido
+#'@param b outro número a subtrair
+#'@examples
+#'subtrair (4,2)
+#'resposta 2
+#'
+#'
+#'@export
+
+subtrair <- function(a,b){
+  a-b
+}
